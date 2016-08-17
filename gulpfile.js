948adbdd552c4cfe9b2d3ca914dcbbf8e3bc44b9
tropinkavray <mcsvyat@yandex.ru>
2016-08-17 16:07:56 +0300

    create header html

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('sass', function() {
 
 // Watchers
 gulp.task('watch', function() {
-  gulp.watch('app/sass/**/*.scss', ['sass']);
+  gulp.watch('app/sass/**/*.sass', ['sass']);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/**/*.js', browserSync.reload);
 });
